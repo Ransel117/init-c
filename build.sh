e562787fa2c22b929dc5580a97714edc5c7b9027
Ransel117 <95185065+Ransel117@users.noreply.github.com>
2023-03-06 22:54:46 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,5 +14,10 @@ CLIBS=""
 
 EXEC="main"
 
+echo "--------COMPILING--------"
+echo $CC $CFLAGS $CDEFINES $CINCLUDES $CFILES -o $EXEC $CLIBS
 $CC $CFLAGS $CDEFINES $CINCLUDES $CFILES -o $EXEC $CLIBS
+
+echo "---------RUNNING---------"
+echo ./$EXEC
 ./$EXEC
